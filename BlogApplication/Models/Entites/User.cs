5763403fa661311eb7396ba18156aa410f78c84c
unknown <whitecircles.VM@gmail.com>
2018-10-11 16:13:44 +0300

    .

---
+++ BlogApplication/Models/Entites/User.cs
@@ -0,0 +1,16 @@
+﻿using BlogApplication.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogApplication.Models.Entites
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+   
+    }
+}
---
+++ dbo.Users.data.sql
@@ -0,0 +1,1 @@
+INSERT INTO [dbo].[Users] ([Id], [name], [age]) VALUES (1, N'Kolya     ', 21)
